05ebf9d58fa832cba0369b193a2b29db293fce2d
egashok <55044715+Egashok@users.noreply.github.com>
2024-02-03 18:21:41 +0600

    realize full

--- js/slider.js
+++ js/slider.js
@@ -8,39 +8,40 @@ const dots=document.querySelector('.slider__dots')
 
 let activeItem=0
 let handlerClick=true   
-let dotsIndex=0
+let scrollMany=false
 
 
 function itemInit(){
     for(var i of  Array.from(sliderItems.children))
-{
+    {
         images.push(i.src.split('img/')[1])
-
     }  
+    const img=document.createElement('img')
+    img.src='./img/'+images[0]
+    img.className="slider__item";
+    sliderItems.append(img)
     nextGenerate()
     prevGenerate()
 
-
-
 }
-function select(){
-    if(this.attr>activeItem){
-       
-        for(var i=Math.abs(this.attr-activeItem)-1;i>=0;i--){
-            
-         nextSlide()
-            
-         handlerClick=true
-    
+function select() {
+    scrollMany=true
+    let diff = Math.abs(this.attr - activeItem);
+    if (this.attr > activeItem) {
+        for (var i = diff - 1; i >= 0; i--) {
+            nextSlide()
+            handlerClick=true
         }
- 
-    }else
-    for(var i=Math.abs(this.attr-activeItem)-1;i>=0;i--){   
-        pervSlide()
-     handlerClick=true
-
+    } else {
+        for (var i = diff - 1; i >= 0; i--) {
+            pervSlide();
+            handlerClick=true
+        }
     }
+    scrollMany=false
+    handlerClick = true;
 }
+
 
 function dotsInit(){
 
@@ -58,103 +59,106 @@ function dotsInit(){
 indicators()}
 itemInit()
 dotsInit()
-
-
-
-
 function nextGenerate(){
+    let nextItem=activeItem+1
+    if(nextItem>=images.length){
+        nextItem=0
+    }
     const img=document.createElement('img')
-    img.src='./img/'+images[activeItem]
+    img.src='./img/'+images[nextItem]
     img.className="slider__item";
-    
     sliderItems.append(img)
-
 }
-function prevGenerate(){
+function prevGenerate(w=false){
     let prevItem =activeItem-1
     if(prevItem<0){
         prevItem=images.length-1
     }
     const img=document.createElement('img')
-    img.style.width="0";
     img.style.height="400px";
+    img.style.width="400px";
+    if(w) img.style.width="0px";
 
     img.src='./img/'+images[prevItem]
     sliderItems.prepend(img)
+
 }
 
-
-
 function nextSlide(){
-
-
     if(handlerClick){
-        
         activeItem++
-    if(activeItem>=images.length){
-        activeItem=0
-    }
-    console.log("siuuuu")
-    nextGenerate()
-
-    right()
-    indicators();
-
+        if(activeItem>=images.length){
+            activeItem=0
+        }
+      let del= document.querySelector('.slider__items img')
+        right(del)
+        nextGenerate()
+        indicators()
     }
     }
 
-    function pervSlide(){
-        if(handlerClick){
+function pervSlide(){
+    if(handlerClick){
+
         activeItem--
         if(activeItem<0){
             activeItem=images.length-1
         }
-        prevGenerate()
-        left()
+        let del=document.querySelector('.slider__items img:last-child')
+        prevGenerate(true)
+        left(del)
         indicators()
+        
     }
-        }
+    }
 
-function right(){
-    handlerClick=false
+function right(del){
+  if(!scrollMany){
     let pos = 0;
     const id = setInterval(frame, 5); 
-    let del= document.querySelector('.slider__items img:nth-child(2)')
-   
+            handlerClick=false
 
     function frame (){ 
     if (pos == 400) {
         clearInterval(id);
         handlerClick=true
-       del.remove()
-
+        del.remove()
 
     } else {
-        pos+=5; 
-
+        pos+=5
         del.style.width=(400-pos)+'px'
 
     }
+  }
+    }
+    else{
+            del.remove()
+        
     }
 }
-function left(){
-    handlerClick=false
-    let elem=document.querySelector('.slider__items img:nth-child(2)')   
-     let pos = 400;
+function left(del){
+    let elem=document.querySelector('.slider__items img')
+    if(!scrollMany){
+        handlerClick=false
+        let pos = 400;
     const id = setInterval(frame, 1); 
-    let del=document.querySelector('.slider__items img:last-child')
 
     function frame (){ 
   
     if (pos == 0) {
         clearInterval(id);
-        handlerClick=true
-        del.remove()     
+        handlerClick=true    
+        del.remove()
           
     } else {
         pos-=5;
         elem.style.width=400-pos+'px'
     }
+    }
+    }else {
+        elem.style.width="400px"
+        del.remove()
+
     }
 }
 
@@ -167,7 +171,6 @@ function indicators(){
 
     doters[activeItem].className += ' active';
 }
-
 
 
 nextBtn.addEventListener("click",nextSlide)
