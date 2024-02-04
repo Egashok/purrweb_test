47201682ef66646112178540abf08aad3a180b4d
egashok <55044715+Egashok@users.noreply.github.com>
2024-02-04 20:49:40 +0600

    realize task

--- js/index.js
+++ js/index.js
@@ -1,100 +1,167 @@
-const cookBtns=document.querySelectorAll('.cookies__btns')
-const cookies=document.querySelector('.cookies')
-const thank=document.querySelector('.thank')
-const form=document.querySelector('.modal')
-const contactBtns=document.querySelectorAll('.contact__btn')
+const cookBtns = document.querySelectorAll('.cookies__btns')
+const cookies = document.querySelector('.cookies')
+const thank = document.querySelector('.thank')
+const form = document.querySelector('.modal')
+const contactBtns = document.querySelectorAll('.contact__btn')
 
 
-let validSubmit=null
 
-for(let i of contactBtns){
-  i.addEventListener("click",openContact)
+
+
+for (let i of cookBtns) {
+  i.addEventListener("click", closeCookies)
 }
-function openContact(){
-  form.style.display="flex"
-}
-
-function closeContact(){
-  form.style.display="none"
+function closeCookies() {
+  cookies.style.display = 'none'
 
 }
-
-function submitForm(){
-
-  validateForm()
-
-  if(validSubmit ){
-    form.style.display="none"
-  thank.style.display="block"
-  }
-
-
+function closeThank() {
+  thank.style.display = "none"
 }
-
-
-for(let i of cookBtns){
-  i.addEventListener("click",closeCookies)
-}
-function closeCookies(){
-  cookies.style.display='none'
-
-}
-function closeThank(){
-  thank.style.display="none"
-}
-
-function validateForm() {
-      let name = document.getElementById('name').value;
-      let email = document.getElementById('email').value;
-      let phone = document.getElementById('phone').value;
-      const nameInp = document.getElementById('name');
-      const emailInp = document.getElementById('email');
-      const phoneInp = document.getElementById('phone');
-
-      let fieldsVal=[name,email,phone]
-
-      let fields=[nameInp,emailInp,phoneInp]
-      let error=false
-
-  console.log(validSubmit)
-  for(var i=0; i<fieldsVal.length;i++){
-    
-    if(fieldsVal[i].trim() === '' ){
-      if(validSubmit || validSubmit==null){
-        let error=document.createElement('p')
-      error.innerHTML ="This field is required."
-      error.classList.add('modal__required')
-      fields[i].parentNode.insertBefore(error, fields[i].nextSibling)
-      }
-      error=true
-    }
-  }
-  if(error){
-    validSubmit=false
-  }
-  else{
-    validSubmit=true
-  }
-
-
-  
-}
-
 
 const menuToggle = document.querySelector('.menu-toggle');
 const menuList = document.querySelector('.menu-list');
 const menu = document.querySelector('.menu');
 const close = document.querySelector('.menu__close');
+const body = document.querySelector('body');
 
 menuToggle.addEventListener('click', () => {
   menuList.classList.toggle('open');
   menu.classList.toggle('open');
-  close.style.display="block"
+  close.style.display = "block"
+  body.style.overflow = "hidden"
 });
 
 close.addEventListener('click', () => {
   menuList.classList.toggle('open');
   menu.classList.toggle('open');
-  close.style.display="none"
+  close.style.display = "none"
+  body.style.overflow = "auto"
+
 
 });
+
+window.addEventListener('scroll', function () {
+  var header = document.querySelector('.hero__top');
+  var scrollPosition = window.scrollY || window.pageYOffset;
+
+  if (scrollPosition >= 100) {
+    header.classList.add('fixed-header');
+  } else {
+    header.classList.remove('fixed-header');
+  }
+});
+
+document.getElementById('name').addEventListener('input', validateForm);
+document.getElementById('email').addEventListener('input', validateForm);
+document.getElementById('phone').addEventListener('input', validateForm);
+
+var nameInput = document.getElementById('name');
+var emailInput = document.getElementById('email');
+var phoneInput = document.getElementById('phone');
+function validateForm() {
+  var submitButton = document.querySelector('.modal__btn');
+
+  if (nameInput.value !== '' && emailInput.value !== '' && phoneInput.value !== '') {
+
+    submitButton.classList.add('valid');
+  } else {
+    submitButton.classList.remove('valid');
+  }
+}
+
+function submitForm() {
+
+  showForm()
+
+  if (nameInput.value !== '' && emailInput.value !== '' && phoneInput.value !== '') {
+
+    form.style.display = "none"
+    thank.style.display = "block"
+  }
+
+
+
+}
+
+const nameReq = document.querySelector('.modal__required-name');
+const emailReq = document.querySelector('.modal__required-email');
+const phoneReq = document.querySelector('.modal__required-phone');
+const fullReq = document.querySelector('.modal__required-generics');
+
+
+function showForm() {
+  if (nameInput.value !== '') {
+    nameReq.style.display = "none"
+    nameInput.classList.remove("input__required")
+
+  }
+  else {
+    nameReq.style.display = "block"
+    nameInput.classList.add("input__required")
+
+  }
+  if (emailInput.value !== '') {
+    emailReq.style.display = "none"
+    emailInput.classList.remove("input__required")
+
+  }
+  else {
+    emailReq.style.display = "block"
+    emailInput.classList.add("input__required")
+
+
+  }
+  if (phoneInput.value !== '') {
+
+    phoneReq.style.display = "none"
+    phoneInput.classList.remove("input__required")
+
+  }
+  else {
+    phoneReq.style.display = "block"
+    phoneInput.classList.add("input__required")
+
+  }
+  if (nameInput.value !== '' && emailInput.value !== '' && phoneInput.value !== '') {
+
+    fullReq.style.display = "none"
+  }
+  else {
+    fullReq.style.display = "block"
+  }
+
+  let error = false
+
+
+  if (error) {
+    validSubmit = false
+  }
+  else {
+    validSubmit = true
+
+  }
+
+
+
+}
+
+document.querySelector('.modal__btn').addEventListener('click', function () {
+
+});
+
+let validSubmit = null
+
+for (let i of contactBtns) {
+  i.addEventListener("click", openContact)
+}
+
+function openContact() {
+  form.style.display = "flex"
+}
+
+function closeContact() {
+  form.style.display = "none"
+
+}
+
